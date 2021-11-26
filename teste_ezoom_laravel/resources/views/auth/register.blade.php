fc7f3649b7ab23874e1ca5342ce780688cb5063b
caioreisalvarenga <caioreisalvarenga@gmail.com>
2021-11-26 17:55:44 -0300

    Correções de português

--- teste_ezoom_laravel/resources/views/auth/register.blade.php
+++ teste_ezoom_laravel/resources/views/auth/register.blade.php
@@ -64,7 +64,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Registrar meu usuário') }}
+                                    {{ __('Registrar usuário') }}
                                 </button>
                             </div>
                         </div>
