b9e7e6b9a0c123f504c3323b99977d8b64213710
caioreisalvarenga <caioreisalvarenga@gmail.com>
2021-11-26 16:15:23 -0300

    ConfiguraçõesRotas (Página inicial)

--- teste_ezoom_laravel/routes/web.php
+++ teste_ezoom_laravel/routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('/home');
+Route::group(['middleware' => 'web'], function(){
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+    Auth::routes();
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
