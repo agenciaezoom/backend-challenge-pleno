7e6f2318ea3201397674666903edddb928954d02
caioreisalvarenga <caioreisalvarenga@gmail.com>
2021-11-27 14:31:15 -0300

    Formatando Front-end

--- teste_ezoom_laravel/resources/views/usuarios/list.blade.php
+++ teste_ezoom_laravel/resources/views/usuarios/list.blade.php
@@ -1,20 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="card-body">
-            @if(session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status')}}
-                </div>
-            @endif
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card-body">
+                    <div class="card-header"><a href="{{ url('usuarios/new') }}">Novo Usuário</a></div>
+                    @if(session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status')}}
+                        </div>
+                    @endif
 
-            <h1>Lista dos usuarios</h1>
-            @foreach( $usuarios as $u )
-                <p>{{ $u->name }} | {{$u->email}}</p>
-            @endforeach
+                    <h1>Lista dos usuarios</h1>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">E-mail</th>
+                                <th scope="col">Editar</th>
+                                <th scope="col">Deletar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                    @foreach( $usuarios as $u )
+                            <tr>
+                                <th scope="row">{{ $u->id }}</th>
+                                <td>{{ $u->name }}</td>
+                                <td>{{ $u->email }}</td>
+                                <td>
+                                    <a href="{{ url('usuarios/{{ $u->id }}/edit') }}" class="btn btn-info">Editar</a>
+                                </td>
+                                <td>
+                                    <button class="btn btn-danger">Deletar</button>
+                                </td>
+                            </tr>
+                    @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
