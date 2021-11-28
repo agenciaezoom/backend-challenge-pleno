c45676e1bfa92a61398c6031b9e303b2edfd2412
caioreisalvarenga <caioreisalvarenga@gmail.com>
2021-11-28 14:35:48 -0300

    Criando a tabela de produtos

---
+++ teste_ezoom_laravel/database/migrations/2021_11_28_151952_create_produtos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('nome_produto', 128);
+            $table->integer('quantidade');
+            $table->float('preco');
+            $table->timestamps();
+
+          
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
